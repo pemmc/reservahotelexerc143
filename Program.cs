eb52e6bf8579728a5b5d15d86a8d6a469ea78c35
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-17 13:06:53 -0300

    INCÍO

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReservaHotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
