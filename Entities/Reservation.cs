77b5e83fdca4282e28940f889c9ce0b5de92df18
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-17 14:01:08 -0300

    sem o tratamento das excecoes... tudo noprograma principal

---
+++ Entities/Reservation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ReservaHotel.Entities
+{
+    public class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+            //construtor padrao
+        }
+
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            //construtor com argumentos
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+
+            return (int)duration.TotalDays;
+
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+
+        }
+
+        public override string ToString()
+        {
+
+            return "Room "
+                + RoomNumber
+                + ". CheckIn: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ". CheckOut: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+
+using ReservaHotel.Entities;
 
 namespace ReservaHotel
 {
@@ -6,8 +9,71 @@ namespace ReservaHotel
     {
         static void Main(string[] args)
         {
+            CultureInfo idiomaptBR = new CultureInfo("pt-BR");
 
-            Console.WriteLine("Hello World!");
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("Olá! Hoje é/ " + now.ToString("D", idiomaptBR));
+            Console.WriteLine();
+
+            Console.WriteLine("Room number: ");
+
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+
+            DateTime checkIn = DateTime.Parse(Console.ReadLine(), idiomaptBR);
+ 
+            Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+
+            DateTime checkOut = DateTime.Parse(Console.ReadLine(), idiomaptBR);
+
+            if(checkOut <= checkIn)
+            {
+                Console.WriteLine("Data de saída tem que ser depois da data de checkin");
+
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+
+                Console.WriteLine("Resevation: " + reservation);
+                Console.WriteLine();
+
+                Console.WriteLine("Entre com os dados para atualizar a reserva");
+
+                Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+
+                checkIn = DateTime.Parse(Console.ReadLine(), idiomaptBR);
+
+                Console.WriteLine("Check-out date (dd/MM/yyyy): ");
+
+                checkOut = DateTime.Parse(Console.ReadLine(), idiomaptBR);
+
+               
+                if (checkIn < now || checkOut < now)
+                {
+
+                    Console.WriteLine("Erro na reserva");
+
+
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Data de saída tem que ser depois da data de checkin");
+
+                }
+                else
+                {
+
+                    reservation.UpdateDates(checkIn, checkOut);
+
+                    Console.WriteLine("Reservation: " + reservation);
+
+
+                }
+
+            }
 
         }
     }
